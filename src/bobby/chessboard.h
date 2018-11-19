08e7285091b3f4b3c5c134e5086ed53d559ed76c
Konstantin Wohlwend <wohlwenk@student.ethz.ch>
2018-11-19 19:36:26 +0100

    Added ChessBoard::toHumanReadable for a visualization of the chessboard.

--- src/bobby/chessboard.h
+++ src/bobby/chessboard.h
@@ -8,19 +8,36 @@
 struct BoardPosition {
     int column;
     int row;
-    std::string toString();
+
+    BoardPosition(int column, int row);
+
+    // Implicit casting from/to C++ strings
+    BoardPosition(const std::string& str);
+    BoardPosition& operator= (const std::string& str);
+    operator std::string();
+
+    // Implicit casting from C strings (for literals)
+    BoardPosition(const char* str);
+    BoardPosition& operator= (const char* str);
 };
 
-struct ChessBoard {
-    BoardSquare squares[8][8];
-    ChessBoard();
-    BoardSquare getSquare(BoardPosition position);
-};
-
-struct ChessMove {
+struct BoardMove {
     BoardPosition from;
     BoardPosition to;
-    std::string toString(ChessBoard board);
+
+    BoardMove(BoardPosition from, BoardPosition to);
+};
+
+class ChessBoard {
+    BoardSquare squares[8][8];
+
+    public: 
+    ChessBoard();
+
+    inline BoardSquare& operator[](BoardPosition position);
+    bool isLegal(BoardMove move);
+
+    std::string toHumanReadable(bool ansi=false);
 };
 
 #endif  // BOBBY_TRAWLER_BOBBY_CHESSBOARD_H_
